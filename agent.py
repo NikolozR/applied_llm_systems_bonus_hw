0a6cbd31044ea1bd7288f4c81874262b200f1d18
NikolozR <nika.rusishvili.95@gmail.com>
2025-12-07 02:05:59 +0400

    added comments

--- agent.py
+++ agent.py
@@ -6,21 +6,23 @@ from google.genai import types
 from tools import add_employee, delete_employee, get_id_by_name
 from schemas import add_employee_tool_schema, delete_employee_tool_schema, get_id_by_name_tool_schema
 
+
+
 load_dotenv()
-
 api_key = os.getenv("LLM_HW_API_KEY")
-
 if not api_key:
     raise ValueError("API Key not set")
 
 client = genai.Client(api_key=api_key)
 
+# This is tool mapping, so when LLM returns function name we can easily call it
 tool_mapping = {
     'add_employee': add_employee,
     'delete_employee': delete_employee,
     'get_id_by_name': get_id_by_name
 }
 
+# Just list of tool schemas that LLM can use
 llm_tools = [
     add_employee_tool_schema,
     delete_employee_tool_schema,
@@ -30,9 +32,13 @@ llm_tools = [
 tools = types.Tool(function_declarations=llm_tools) # type: ignore
 
 config = types.GenerateContentConfig(tools=[tools])
-
+# we use chat so in terminal we can just simply create chat simulation
 chat = client.chats.create(model="gemini-2.5-flash", config=config)
 
+# I later analyzed that this kind of implementation (recursive) gave my system ability
+# to chain several tool calling based on previous response
+# P.S. there is some ways to implement this with Gemini API configs (for example parallel tool calling), but I find my solution pretty and fun
+# Even though my solution makes more api calls
 def execute_agent(raw_query):
     response = chat.send_message(raw_query)
     if response.candidates[0].content.parts[0].function_call: # type: ignore
